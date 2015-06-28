1af24f49b1aa0cc8b10cd4ca330a43d17e6f3074
Steve White <stevewhite16@gmail.com>
2015-06-27 20:41:44 -0400

    Updated .gitignore
    Now ignore .Rhistory file and updated PC R file

--- principle_components.R
+++ principle_components.R
@@ -1,6 +1,8 @@
 # Principle Components of the Risk Scores
 library(nsprcomp)
-data <- read.csv("~/Dropbox/Research/Dot House/R files/dothouse_high_risk.csv")
+
+# setwd("GitHub/Dothouse")
+data <- read.csv("data/dothouse_high_risk.csv")
 
 risk_vars <- c(-1,-2,-3,-4,-5,-42,-43,-44,-53,-54,-55)
 risk_scores <- data[,risk_vars]
@@ -9,3 +11,4 @@ risk_scores[NAs] <- 0
 
 pc <- nsprcomp(x = risk_scores, ncomp = 5, scale. = TRUE)
 summary(pc)
+
