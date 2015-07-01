bd9e51d7eaa85cfe248c662f615f057164d66891
Steve White <stevewhite16@gmail.com>
2015-06-30 22:37:55 -0400

    Modified er use w/ tree model
    minor modifications

--- tree_eruse.R
+++ tree_eruse.R
@@ -12,21 +12,23 @@ predictors <- data[,risk_vars]
 NAs <- is.na(predictors)
 predictors[NAs] <- 0
 
-# regsubsets(formula, data=, nvmax=)
 
-library(glmnet)
-set.seed(1)
-x <- model.matrix(~., predictors)
 novisit <- data[,4]
-data <- data.frame(novisit,x)
-n = nrow(x)
+data <- data.frame(novisit,predictors)
+n = nrow(data)
 train <- sample(1:n, n/2, replace=FALSE) # pick a training set
 test <- (-train)
 
-hosp.tree <- tree(formula = novisit ~ ., data = data, subset = train)
-plot(hosp.tree)
-text(hosp.tree, pretty=0)
+tree <- tree(formula = novisit ~ ., data = data, subset = train)
+plot(tree)
+text(tree, pretty=0)
 
+high_er_use <- (novisit >= 10)
+data <- data.frame(high_er_use, predictors)
+
+tree <- tree(formula = high_er_use ~ ., data = data, subset = train)
+plot(tree)
+text(tree, pretty=0)
 
 # set.seed(1)
 # cv.hosp.tree <- cv.tree(hosp.tree, FUN=prune.misclass)
